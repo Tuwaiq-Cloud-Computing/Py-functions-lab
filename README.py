382b3ef7947513eefef1a57ce8d815316114b99b
iRavvan <114053471+iRavvan@users.noreply.github.com>
2022-09-26 21:41:42 +0300

    Rawan Alqahtani

---
+++ README.py
@@ -0,0 +1,9 @@
+# Py-function-lab - lab 3
+
+- Question 1: Write a function that takes two numbers and return their sum 
+- Question 2: Prints all the elements in the list using a for loop
+- Question 3: Write a Python program to sum all the items in the list
+- Question 4: Write a Python program to get the largest number from the list
+- Question 5: write a function that take a list and a number then return a new partial list starting from index 0 to index "number"
+- Question 6: Loop through the letters in the string: "Tuwaiq_Academy
+- Question 7: Consider this ``` list = ["Python", "C++", "Java"] ``` Exit the loop when x is equal to "C++"
